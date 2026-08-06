af26c93c0b064cbc3a31bf4bf20ae13a5b790715
Haris Skiadas <cskiadas@gmail.com>
2026-08-06 18:06:19 -0400

    Show full course names in day and slot drill-down views

--- components/CoursePill.js
+++ components/CoursePill.js
@@ -1,4 +1,7 @@
 import { goScheduleCourse, goScheduleInstructor } from '../lib/router.js'
+import { allCourses } from '../lib/store.js'
+
+const { computed } = Vue
 
 export default {
   name: 'CoursePill',
@@ -7,18 +10,23 @@ export default {
     filterActive: { type: Boolean, default: false },
     color: { type: String, default: '' },
   },
-  setup() {
-    return { goScheduleCourse, goScheduleInstructor }
+  setup(props) {
+    const courseName = computed(() =>
+      allCourses.value[props.item.code] ? allCourses.value[props.item.code].course_name : '',
+    )
+    return { goScheduleCourse, goScheduleInstructor, courseName }
   },
   template: `
     <span
       class="slot-pill"
       :class="{ 'filter-colored': filterActive }"
       :style="filterActive ? { backgroundColor: color } : {}"
+      :title="courseName"
     >
       <span class="slot-pill-code" @click="goScheduleCourse(item.code)">
         {{ item.code }}<span class="sep">{{ item.o.section }}</span>
       </span>
+      <span class="slot-pill-name">{{ courseName }}</span>
       <span class="slot-pill-inst" @click="goScheduleInstructor(item.o.instructor)">
         {{ item.o.instructor }}
       </span>
